9f3fdf5373447ccfaa64193abfd22af1c95ac0ca
julissan <55885274+julissan@users.noreply.github.com>
2020-09-21 18:22:29 -0500

    Add files via upload

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -2,28 +2,28 @@ var express = require('express');
 var router = express.Router();
 const postController = require ('../controllers/post.controller'); 
 /**
- * GET Route to list all users
+ * GET Route to list all posts
  */
 router.get('/', postController.findAllPosts);
 
 /**
- * GET Route to find user by id
+ * GET Route to find post by id
  */
 router.get('/:idUser', postController.findOnePost);
 /**
- * POST Route to create user
+ * POST Route to create post
  */
 router.post ('/',postController.createPost);
 /**
- * PUT Route to update an user by id
+ * PUT Route to update a post by id
  */
 router.put ('/:idUser',postController.updatePost);
 /**
- * DELETE Route to delete all users
+ * DELETE Route to delete all posts
  */
 router.delete ('/',postController.deleteAllPosts);
 /**
- * GET Route to find All Users By Created Date
+ * GET Route to find All Posts By PublishedDate
  */
 router.get ('/:creation_date',postController.findAllPostsByPublishedDate);
 
