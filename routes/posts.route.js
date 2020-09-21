9918e0e04a43e236a684ad1552f4845d26547a18
julissan <55885274+julissan@users.noreply.github.com>
2020-09-21 17:56:05 -0500

    Add files via upload

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -4,32 +4,28 @@ const postController = require ('../controllers/post.controller');
 /**
  * GET Route to list all users
  */
-router.get('/', postController.findAllUsers);
+router.get('/', postController.findAllPosts);
 
 /**
  * GET Route to find user by id
  */
-router.get('/:idUser', postController.findOneUser);
+router.get('/:idUser', postController.findOnePost);
 /**
  * POST Route to create user
  */
-router.post ('/',postController.createUser);
+router.post ('/',postController.createPost);
 /**
  * PUT Route to update an user by id
  */
-router.put ('/:idUser',postController.updateUser);
-/**
- * DELETE Route to delete an user by username
- */
-router.delete ('/:username',postController.deleteUserByUsername);
+router.put ('/:idUser',postController.updatePost);
 /**
  * DELETE Route to delete all users
  */
-router.delete ('/',postController.deleteAllUsers);
+router.delete ('/',postController.deleteAllPosts);
 /**
  * GET Route to find All Users By Created Date
  */
-router.get ('/:creation_date',postController.findAllUsersByCreatedDate);
+router.get ('/:creation_date',postController.findAllPostsByPublishedDate);
 
 // Export router
 module.exports = router;
